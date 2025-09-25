688f07cf1ddc0e93ee350ce31562f5068fd690f7
Omar <jabour.omar@hotmail.com>
2025-09-25 11:44:26 +0200

    :sparkles: adding import in App  :sparkles:

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,18 @@
-
+import { useState } from "react";
+import { Container } from "react-bootstrap";
+import QuizAccordion from "./components/QuizAccordion/Quiz.jsx";
 
 function App() {
+  const [category, setCategory] = useState("Toutes");
  
 
   return (
     <>
      <main>
-      
+       <Container className="mt-4">
+        <h1 className="mb-4">Quiz Interactif</h1>
+        <QuizAccordion category={category} />
+      </Container>
      </main>
     </>
   )
